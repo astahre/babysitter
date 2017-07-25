dec05280db0011ff146f642abfb917f65fb6622e
Angel Thomas <athomas@bex.net>
2017-07-24 22:19:38 -0400

    Refactored test class

--- src/EarningsCalculatorTest.java
+++ src/EarningsCalculatorTest.java
@@ -1,19 +1,25 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class EarningsCalculatorTest {
+	
+	private EarningsCalculator ec;
+	
+	@Before
+	public void beforeEachTest() {
+		ec = new EarningsCalculator();
+	}
 
 	@Test
 	public void oneRegularHourEarns12Dollars() {
-		EarningsCalculator ec = new EarningsCalculator();
 		assertEquals(12, ec.calculateEarnings(1, 0, 0));
 	}
 	
 	@Test
 	public void twoRegularHoursEarns24Dollars() {
-		EarningsCalculator ec = new EarningsCalculator();
 		assertEquals(24, ec.calculateEarnings(2, 0, 0));
 	}
 
