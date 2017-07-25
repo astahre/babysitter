d5aadfce49800716bb8afd67253f87191bedd596
Angel Thomas <athomas@bex.net>
2017-07-24 21:56:35 -0400

    Changed the default unit test to be a valid test for one hour of regular wages

--- src/EarningsCalculatorTest.java
+++ src/EarningsCalculatorTest.java
@@ -6,8 +6,9 @@ import org.junit.Test;
 public class EarningsCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void oneRegularHourEarns12Dollars() {
+		EarningsCalculator ec = new EarningsCalculator();
+		assertEquals(12, ec.calculateEarnings(1, 0, 0));
 	}
 
 }
