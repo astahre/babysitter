2fb29884034253614edf23b5ded05be2c9d8017c
Angel Thomas <athomas@bex.net>
2017-08-02 21:43:56 -0400

    Added a 2nd test and logic to calculate hours

--- src/HoursCalculation.java
+++ src/HoursCalculation.java
@@ -1,9 +1,17 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class HoursCalculation {
 
-	public int[] calculateHours(String inTime, String outTime, String bedTime) {
-		int[] array = {3, 0, 0};
-		return array;
+	public long[] calculateHours(String inTime, String outTime, String bedTime) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("hh a");
+		LocalTime timeIn = LocalTime.parse(inTime, formatter );
+		LocalTime timeOut = LocalTime.parse(outTime, formatter);
+		Duration regularHours = Duration.between(timeIn, timeOut);
+		long[] hoursPerPayrate = {regularHours.toHours(), 0, 0};
+		return hoursPerPayrate;
 	}
 
 
--- src/HoursCalculationTest.java
+++ src/HoursCalculationTest.java
@@ -8,9 +8,17 @@ public class HoursCalculationTest {
 	@Test
 	public void timeIn5TimeOut8NoBedtimeEquals3RegularHours() {
 		HoursCalculation hc = new HoursCalculation();
-		int[] expected = new int[3];
+		long[] expected = new long[3];
 		expected[0] = 3;
 		assertArrayEquals(expected, hc.calculateHours("05 PM", "08 PM", "09 PM"));
+	}
+	
+	@Test
+	public void timeIn5TimeOut9NoBedtimeEquals4RegularHours() {
+		HoursCalculation hc = new HoursCalculation();
+		long[] expected = new long[3];
+		expected[0] = 4;
+		assertArrayEquals(expected, hc.calculateHours("05 PM", "09 PM", "09 PM"));
 	}
 
 }
