8d52027a0be5480fe21454c6ce8d800fc21d83b6
Angel Thomas <athomas@bex.net>
2017-08-02 21:53:23 -0400

    Refactor test class

--- src/HoursCalculationTest.java
+++ src/HoursCalculationTest.java
@@ -1,22 +1,28 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class HoursCalculationTest {
+	
+	private HoursCalculation hc;
+	private long[] expected;
+	
+	@Before
+	public void beforeEachTest() {
+		hc = new HoursCalculation();
+		expected = new long[3];
+	}
 
 	@Test
 	public void timeIn5TimeOut8NoBedtimeEquals3RegularHours() {
-		HoursCalculation hc = new HoursCalculation();
-		long[] expected = new long[3];
 		expected[0] = 3;
 		assertArrayEquals(expected, hc.calculateHours("05 PM", "08 PM", "09 PM"));
 	}
 	
 	@Test
 	public void timeIn5TimeOut9NoBedtimeEquals4RegularHours() {
-		HoursCalculation hc = new HoursCalculation();
-		long[] expected = new long[3];
 		expected[0] = 4;
 		assertArrayEquals(expected, hc.calculateHours("05 PM", "09 PM", "09 PM"));
 	}
