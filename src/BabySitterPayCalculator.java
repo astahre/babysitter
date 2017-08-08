1f543728b6c53634c7dd811d0c98928fc95dc5be
Angel Thomas <athomas@bex.net>
2017-08-08 15:36:25 -0400

    Add scanner to accept input and to convert the inputted integer to a string of a 2 digit number

--- src/BabySitterPayCalculator.java
+++ src/BabySitterPayCalculator.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 
 public class BabySitterPayCalculator {
 
 	public static void main(String[] args) {
 		String outputMessage = "test";
 		System.out.println("Enter the starting hour of your shift:");
+		Scanner scanner = new Scanner(System.in);
+		int inputInTime = scanner.nextInt();
+		String inTime = String.format("%02d", inputInTime);
+		outputMessage = inTime;
 		System.out.println(outputMessage);
 	}
 }
