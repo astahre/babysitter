72030ea3b52328aa026446f0341bbd412ae4dd32
Angel Thomas <athomas@bex.net>
2017-08-08 10:05:48 -0400

    Refactor unit test class

--- src/InputValidatorTest.java
+++ src/InputValidatorTest.java
@@ -1,19 +1,25 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class InputValidatorTest {
+	
+	private InputValidator iv;
+	
+	@Before
+	public void beforeEachTest() {
+		iv = new InputValidator();
+	}
 
 	@Test
 	public void inTimeOF4pmIsTooEarly() {
-		InputValidator iv = new InputValidator();
 		assertEquals(false, iv.validateTime("04 PM", "08 PM"));
 	}
 	
 	@Test
 	public void inTimeOF5pmIsValid() {
-		InputValidator iv = new InputValidator();
 		assertEquals(true, iv.validateTime("05 PM", "08 PM"));
 	}
 	
