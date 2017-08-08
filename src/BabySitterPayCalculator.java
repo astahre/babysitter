5da0b0dcfb6b2c6001d306cc3b239b44514d83cf
Angel Thomas <athomas@bex.net>
2017-08-08 15:26:20 -0400

    Add a message to enter starting hour

--- src/BabySitterPayCalculator.java
+++ src/BabySitterPayCalculator.java
@@ -3,7 +3,7 @@ public class BabySitterPayCalculator {
 
 	public static void main(String[] args) {
 		String outputMessage = "test";
+		System.out.println("Enter the starting hour of your shift:");
 		System.out.println(outputMessage);
 	}
-
 }
